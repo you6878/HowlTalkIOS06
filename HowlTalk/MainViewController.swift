cbd62c228e1150909390f3c3bf5138c2612136d5
MyeongSic <you6878@icloud.com>
2017-09-05 20:35:45 +0900

    init

---
+++ HowlTalk/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  HowlTalk
+//
+//  Created by 유명식 on 2017. 8. 29..
+//  Copyright © 2017년 swift. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
